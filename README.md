4c03eced42a27d2661ad40d33966e0d14449ed0f
jarvena <antti.p.jarvenpaa@aalto.fi>
2022-06-18 15:38:03 +0300

    Add Venla controls -> leg 2

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A web map of Jukola 2022 terrain. Initialized with qgis2web. Built on top of Ope
 See the map live https://jarvena.github.io/lukkarijukola/
 
 Here is also a [pdf-version](https://jarvena.github.io/lukkarijukola/export.pdf) of the map, e.g., for sketching.
+
+Check also an [interesting 3D visualization of the terrain](https://3d.routegadget.net/?zip=https%3A%2F%2Fwww.dropbox.com%2Fs%2F4n1n12uxt2fjahu%2Fjukola2022oldmap.zip) prepared by [Mr. Routegadget](https://github.com/Jagge)
---
+++ layers/venlaRastit.js
@@ -0,0 +1,29 @@
+var json_rastit = {
+    "type": "FeatureCollection",
+    "name": "venlaRastit",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::3857" } },
+    "features": [
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2450437.130264638457447, 8545117.031120512634516 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2449995.762960962951183, 8544811.683006105944514 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2449778.602226692717522, 8548266.491491302847862 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451714.890932913869619, 8547469.958907436579466 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451325.770363172981888, 8547130.613931659609079 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451865.325363556388766, 8546787.369537640362978 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451193.152065961621702, 8546426.995543073862791 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451958.786746699828655, 8545798.00883018411696 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2452139.754901385866106, 8545465.715240761637688 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2450402.196484552696347, 8545373.47543347813189 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2450437.130264638457447, 8545117.031120512634516 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2449995.762960962951183, 8544811.683006105944514 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2449778.602226692717522, 8548266.491491302847862 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451714.890932913869619, 8547469.958907436579466 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451325.770363172981888, 8547130.613931659609079 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451865.325363556388766, 8546787.369537640362978 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451193.152065961621702, 8546426.995543073862791 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2451958.786746699828655, 8545798.00883018411696 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2452139.754901385866106, 8545465.715240761637688 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2450402.196484552696347, 8545373.47543347813189 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2450216.231387197971344, 8549531.955493841320276 ] } },
+    { "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 2450162.411225277464837, 8549515.483549021184444 ] } }
+    ]
+    }
